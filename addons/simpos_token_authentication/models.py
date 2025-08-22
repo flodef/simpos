76f0d8937cb4a58758b747499fb7d4e88cbae947
Flojito <flojito@archireport.com>
2025-08-22 17:57:48 +0200

    fix: update token auth to use request.update_env for Odoo 18 compatibility

--- addons/simpos_token_authentication/models.py
+++ addons/simpos_token_authentication/models.py
@@ -15,6 +15,7 @@ class IrHttp(models.AbstractModel):
         if token:
             token = token.replace('Bearer ', '')
             payload = jwt.decode(token, request.env['ir.config_parameter'].sudo().get_param('database.secret'), algorithms=["HS256"])
-            request.uid = payload.get('uid')
+            # Use update_env instead of direct uid assignment (Odoo 18 compatibility)
+            request.update_env(user=payload.get('uid'))
         else:
             super(IrHttp, cls)._auth_method_user()
